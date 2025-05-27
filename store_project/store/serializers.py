e7e4ea9c22fc72f85df4de2854bb11c10c28ba84
Ovalga <145358551+Ovalga@users.noreply.github.com>
2025-05-27 18:11:53 +0400

    Update serializers.py

---
+++ store_project/store/serializers.py
@@ -0,0 +1,3 @@
+git add store_project/store/serializers.py
+git commit -m "feat(serializers): добавил сериализаторы для Product и Category"
+git push origin fix/serializers  # или fix/errors, если используете её
