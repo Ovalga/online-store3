dfb604e2bb7093b9569483b01e7f87819a654e5c
Ovalga <145358551+Ovalga@users.noreply.github.com>
2025-05-27 17:57:17 +0400

    Update models.py

--- store_project/store/models.py
+++ store_project/store/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название категории")
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.name} ({self.price} руб.)"
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
